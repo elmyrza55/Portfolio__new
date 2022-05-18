c429bf8e17b61a05873d79ec6019ccd649c2e1f4
elmyrza55 <elmyrza0221@gmail.com>
2022-05-18 17:23:09 +0600

    created model, conne cted DB Postgres, added button, form,name in index.html, created view for message

--- partfolia/urls.py
+++ partfolia/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage
+from main.views import homepage,sendContact
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',homepage,name='homepage'),
+    path('sendContact/',sendContact,name='sendContact'),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
